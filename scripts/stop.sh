7fb2fa81b2e5a775b21bedd553246337fced7f93
Thomas Berger <thomas@epicentral.de>
2017-07-15 11:42:57 +0200

    Added sample stop script based on the PID file

---
+++ scripts/stop.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PID_FILE=run.pid
+if [ -f ${PID_FILE} ]; then
+   PID=`cat ${PID_FILE}`
+   echo "Stopping process with PID $PID"
+   sudo su cinevote -c 'kill $PID'
+else
+   echo "Cannot stop. $PID_FILE does not exist."
+fi
