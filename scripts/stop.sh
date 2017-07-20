4ec49ddd0fbee204a86a3d2c6e3d44855080e6ff
Thomas Berger <thomas@epicentral.de>
2017-07-20 22:33:58 +0200

    Fixed stop script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,7 +4,7 @@ PID_FILE=run.pid
 if [ -f ${PID_FILE} ]; then
    PID=`cat ${PID_FILE}`
    echo "Stopping process with PID $PID"
-   sudo su cinevote -c 'kill $PID'
+   sudo -E -u cinevote kill ${PID}
 else
    echo "Cannot stop. $PID_FILE does not exist."
 fi
